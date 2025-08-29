73f4eed2421c7153600be51b7a4ca46d68451654
Francis Carolina <francis.carolinalovo@gmail.com>
2025-08-29 11:19:32 -0300

    javalin app added

--- src/main/java/br/edu/ifpr/pgua/eic/tads/Main.java
+++ src/main/java/br/edu/ifpr/pgua/eic/tads/Main.java
@@ -5,13 +5,24 @@ import br.edu.ifpr.pgua.eic.tads.models.*;
 import br.edu.ifpr.pgua.eic.tads.controllers.IndexController;
 import br.edu.ifpr.pgua.eic.tads.utils.JavalinUtils;
 import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
 
 public class Main { // simulando banco de dados
-    public static ArrayList<Contato> databaseContato = new ArrayList<>();
+    //public static ArrayList<Contato> databaseContato = new ArrayList<>();
     public static void main(String[] args) {
-        Contato contato = new Contato("augusto", "123@gmail.com", "12344321");
+       /* Contato contato = new Contato("augusto", "123@gmail.com", "12344321");
         Agenda agenda = new Agenda("INFO24", "online");
         agenda.adicionarContato(contato);
-        System.out.println(agenda);
+        System.out.println(agenda);*/
+
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public", Location.CLASSPATH);
+        }).start(7000); 
+
+        app.get("/", ctx -> {
+            ctx.redirect("index.html");
+        });
+
+
     }
 }
